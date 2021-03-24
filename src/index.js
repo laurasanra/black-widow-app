90ecdd5dd2f89af1e1f1a2f28f6f00d95a2706dc
laurasanra <laura.san.rada@gmail.com>
2021-03-24 19:59:37 +0100

    last changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable'
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "raf/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./../src/styles/index.scss";
